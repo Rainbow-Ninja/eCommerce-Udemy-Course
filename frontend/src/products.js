7cfc6a6227cdd46bc28fae73e097dcfcbc541a9d
Jo Dunham <scaffelpike@yahoo.com.au>
2020-12-13 15:25:45 +1100

    card styling added for 6 products brought in with js file

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Kevin',
+    image: '/images/kitty1.jpg',
+    description:
+      'Gorgeous playful bundle of fur. Loves cuddles and headbutting you in the face easly in the morning.',
+    brand: 'CuddleBear',
+    category: 'Medium Kitty',
+    price: 89.99,
+    countInStock: 10,
+    rating: 5.0,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Squeak',
+    image: '/images/kitty2.jpg',
+    description:
+      'She has a lot to say, and loves to tell you all about her day and about her latest lizard hunt. She\'s a sweet little thing and loves to nuzzle up at the end of a long hour of chasing leaves.',
+    brand: 'EnergyBall',
+    category: 'Kitten',
+    price: 109.99,
+    countInStock: 7,
+    rating: 5.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Belford',
+    image: '/images/kitty3.jpg',
+    description:
+      'Belford is a little shy, but enjoys watching movies. Hobbys include zoomies at 3am, chasing seemingly nothing about the house, and informing you about their low food bowl. 10 out of 10, would pat.',
+    brand: 'FluffBall',
+    category: 'Medium Kitty',
+    price: 79.99,
+    countInStock: 5,
+    rating: 4.9,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Pixie',
+    image: '/images/kitty4.jpg',
+    description:
+      'Sweet little girl. Loves belly scitches, but only exactly 7 before she bites. Little ball of energy, and nap expert.',
+    brand: 'EnergyBall',
+    category: 'Kitten',
+    price: 119.99,
+    countInStock: 11,
+    rating: 5.0,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Shamrock',
+    image: '/images/kitty5.jpg',
+    description:
+      'This girl has the most beautiful green eyes! She is more mature in years and loves companionship. Beautiful temperament, and is quite the lap kitty.',
+    brand: 'LapKitty',
+    category: 'Mature',
+    price: 89.99,
+    countInStock: 7,
+    rating: 5.0,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'The Captain',
+    image: '/images/kitty6.jpg',
+    description:
+      'The Captain knows what he wants and will thusly inform you of such. He can be a little difficult to get to know, but is well worth your effort. Loves to stay near you once you are considered one of his crew.',
+    brand: 'Surly',
+    category: 'Medium Kitty',
+    price: 99.99,
+    countInStock: 0,
+    rating: 4.8,
+    numReviews: 12,
+  },
+]
+
+export default products
